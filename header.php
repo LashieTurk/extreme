8d7a4d86b2a114fccc09b2ed91bf98e2c3274871
LashieTurk <lashitork@mail.com.tr>
2021-03-18 15:44:58 +0300

    Update header.php

--- header.php
+++ header.php
@@ -4,14 +4,4 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-hjkhjkghjkg
 
-    
-    tyhgft
-
-    gfh
-    fghd
-    
-    
